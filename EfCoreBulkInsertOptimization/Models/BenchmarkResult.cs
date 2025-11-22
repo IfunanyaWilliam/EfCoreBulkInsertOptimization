ac04850757f6f99dadcacb31525ea3002739a268
IfunanyaWilliam <williamifunanya@gmail.com>
2025-11-22 18:58:00 +0100

    Add models

---
+++ EfCoreBulkInsertOptimization/Models/BenchmarkResult.cs
@@ -0,0 +1,11 @@
+﻿namespace EfCoreBulkInsertOptimization.Models
+{
+    public class BenchmarkResult
+    {
+        public string? Action { get; set; }
+        public int Entities { get; set; }
+        public string? Performance { get; set; }
+        public string? TimeFaster { get; set; }
+        public string? ReducedPercent { get; set; }
+    }
+}
